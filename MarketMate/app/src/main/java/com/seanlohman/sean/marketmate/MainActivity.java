65768c8f0e48a7cf036a33de3c16e34502e9dbce
Sean Lohman <smlohman@fullsail.edu>
2018-07-15 16:04:48 -0400

    updated map fragment

--- MarketMate/app/src/main/java/com/seanlohman/sean/marketmate/MainActivity.java
+++ MarketMate/app/src/main/java/com/seanlohman/sean/marketmate/MainActivity.java
@@ -3,11 +3,17 @@ package com.seanlohman.sean.marketmate;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.seanlohman.sean.marketmate.Fragments.MarketMapFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getFragmentManager().beginTransaction().replace(R.id.frameLayout_Main, MarketMapFragment.newInstance()).commit();
+        
+
     }
 }
