6ff59e97a5a3b65e11f083c747a6572a8471994e
Sean Lohman <smlohman@fullsail.edu>
2018-07-26 19:31:05 -0400

    Final Revisions
    Market Mate, Final Commit for Android Project Class

--- MarketMate/app/src/main/java/com/seanlohman/sean/marketmate/Fragments/GoogleMapsFragment.java
+++ MarketMate/app/src/main/java/com/seanlohman/sean/marketmate/Fragments/GoogleMapsFragment.java
@@ -1,8 +1,10 @@
 package com.seanlohman.sean.marketmate.Fragments;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.location.Location;
@@ -19,6 +21,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -91,7 +94,50 @@ public class GoogleMapsFragment extends SupportMapFragment implements OnMapReady
         if (id == R.id.action_settings) {
             Log.e("Tag","Settings Pressed");
 
-            
+
+            /* Alert Dialog Code Start*/
+            AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+            alert.setTitle("Search By Zipcode"); //Set Alert dialog title here
+            alert.setMessage("Enter a zipcode"); //Message here
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getContext());
+            alert.setView(input);
+
+            alert.setPositiveButton("Search", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+            //You will get as string input data in this variable.
+            // here we convert the input to a string and show in a toast.
+                    String srt = input.getEditableText().toString();
+
+                    if (srt.length() == 5){
+                        new SearchTask().execute(srt);
+
+                        LatLng latLng = new LatLng(38,
+                                -96);
+
+                        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 3);
+
+                        mMap.animateCamera(cameraUpdate);
+
+                    }else{
+                        Toast.makeText(getContext(), "Invalid Zipcode", Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+                    }
+
+                } // End of onClick(DialogInterface dialog, int whichButton)
+            }); //End of alert.setPositiveButton
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+            // Canceled.
+                    dialog.cancel();
+                }
+            }); //End of alert.setNegativeButton
+            AlertDialog alertDialog = alert.create();
+            alertDialog.show();
+            /* Alert Dialog Code End*/
+
+
             return true;
         }
 
@@ -311,6 +357,131 @@ public class GoogleMapsFragment extends SupportMapFragment implements OnMapReady
             }
 
         } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()){
+                pd.dismiss();
+                updateMap();
+            }
+        }
+
+    }
+
+    private class SearchTask extends AsyncTask<String, String, String>{
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = new ProgressDialog(getActivity());
+            pd.setMessage("Loading Market Information");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpsURLConnection connection = null;
+            BufferedReader reader = null;
+            markets = new ArrayList<>();
+
+            try {
+
+                URL url = new URL("https://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + strings[0]);
+                connection = (HttpsURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream is = connection.getInputStream();
+
+                String data = IOUtils.toString(is, "UTF-8");
+
+                is.close();
+
+                connection.disconnect();
+
+
+                JSONObject outterobject = new JSONObject(data);
+                JSONArray marketArray = outterobject.getJSONArray("results");
+
+                markets.clear();
+
+                for (int i = 0; i < marketArray.length(); i++){
+
+                    JSONObject arrayObject = marketArray.getJSONObject(i);
+                    String id = arrayObject.getString("id");
+                    String firstName =  arrayObject.getString("marketname");
+
+                    String name = firstName.substring(firstName.indexOf(" ")+1);
+
+                    //Log.e("MarketName", name);
+
+                    url = new URL("https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id);
+                    connection = (HttpsURLConnection) url.openConnection();
+                    connection.connect();
+
+                    is = connection.getInputStream();
+
+                    data = IOUtils.toString(is, "UTF-8");
+
+                    is.close();
+
+                    connection.disconnect();
+
+                    outterobject = new JSONObject(data);
+                    JSONObject marketObject = outterobject.getJSONObject("marketdetails");
+
+                    String address = marketObject.getString("Address");
+
+                    String productString = marketObject.getString("Products");
+
+                    ArrayList<String> products = new ArrayList<String>(Arrays.asList(productString.split(";")));
+
+                    if (products.size() > 1){
+
+                        String timeString = marketObject.getString("Schedule");
+
+                        String timeEdit = timeString.substring(0, timeString.indexOf(";"));
+
+                        String time = timeEdit.substring(timeEdit.indexOf(":")+1).trim();
+
+                        Log.e("time test ", time);
+
+                        //Log.e("Market: ", "Name: " + name + " products size: " + products.size());
+
+                        url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" + address.replace(' ', '+') + "&key=AIzaSyCKZZdcJBVkFpz_JdhW6AGqOT6YtvYB0AQ");
+                        //Log.e("URL ",url.toString());
+                        connection = (HttpsURLConnection) url.openConnection();
+                        connection.connect();
+
+                        is = connection.getInputStream();
+
+                        data = IOUtils.toString(is, "UTF-8");
+
+                        is.close();
+
+                        connection.disconnect();
+
+                        outterobject = new JSONObject(data);
+                        JSONArray resultsArray = outterobject.getJSONArray("results");
+                        JSONObject innerObject = resultsArray.getJSONObject(0);
+                        JSONObject geometryObject = innerObject.getJSONObject("geometry");
+                        //Log.e("Geometry Object", geometryObject.toString());
+                        JSONObject locationObject = geometryObject.getJSONObject("location");
+                        Double lat = locationObject.getDouble("lat");
+                        Double lng = locationObject.getDouble("lng");
+
+                        Log.e("Market Information ", "Name: " + name + " products size: " + products.size() + " Lat: " + lat + " Lng: " + lng);
+                        markets.add(new Market(name, address, lat, lng, time, products));
+                    }
+
+                }
+
+            } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
             return null;
